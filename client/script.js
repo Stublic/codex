01538d14e4547aa755c0e5d48449ce136802b681
stublic.jurica <stublic.jurica@gmail.com>
2023-05-16 14:11:24 +0200

    addea a live server

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
   // fetch data frome server -> bot's response
 
-  const response = await fetch('http:/localhost:5000', {
+  const response = await fetch('https://codex-9iwm.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'aplication/json'
